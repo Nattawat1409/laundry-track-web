10198afeda5f2f2a35c19942cc44e16b693e41a5
Tai Dev_Tech <nattawat140947@gmail.com>
2026-01-16 00:14:31 +0700

    add button and layout of collection

--- laundry-track/src/App.jsx
+++ laundry-track/src/App.jsx
@@ -12,6 +12,22 @@ function App() {
         <li className='hover:scale-105 transition-all  text-slate-950 hover:text-teal-400'>Account</li>
       </ul>
       </header>
+
+      <div className='lg:w-full h-120 flex items-center justify-center sm:py-20 px-4 '>
+        <div className='bg-white w-400 h-100 rounded-lg'> 
+          <div className=' text-4xl text-center font-semibold py-4'>Collection
+      </div>
+
+      {/* Button */}
+      <div className='flex items-center justify-end py-65 px-5 '>
+          <button className='bg-teal-400 rounded-full px-4 py-2 font-medium text-white hover:bg-teal-800 text-teal-100 hover:scale-108 hover:transition-all'>Save Changes</button>
+      </div>
+      </div>
+       
+
+          
+        
+      </div>
     </div>
   )
 }

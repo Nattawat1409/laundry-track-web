d37cde930c8cb108d0bfdf1aa1b41f489fb33930
Tai Dev_Tech <nattawat140947@gmail.com>
2026-01-15 12:28:05 +0700

    add navbar color and mordern logo

--- laundry-track/src/App.jsx
+++ laundry-track/src/App.jsx
@@ -1,14 +1,15 @@
 import './App.css' 
-import Logo from './assets/Logo.png'
+import Logo from './assets/Logo_Nav.png'
 function App() {
   return (
-    <div className="w-full h-full absolute bg-gradient-to-b from-teal-300 to-teal-100">
-      <header className='flex justify-between items-center text-white py-6 px-8 md:px-32 bg-slate-100 drop-shadow-md'><a href="#"><img src={Logo} alt="Laundry Track Logo" className='w-52 hover:scale-105 transition-all' /></a>
+    // Navbar
+    <div className="w-full h-full absolute bg-gradient-to-r from-teal-200 to-emerald-200">
+      <header className='flex justify-between items-center py-6 px-8 md:px-32 bg-slate-100 drop-shadow-md'><a href="#"><img src={Logo} alt="Laundry Track Logo" className='w-52 hover:scale-105 transition-all' /></a>
       <ul className='hidden xl:flex items-center gap-12 font-semibold text-base '>
-        <li className='hover:scale-105 transition-all text-teal-950'>Laundry</li>
-        <li className='hover:scale-105 transition-all  text-teal-950'>Collections</li>
-        <li className='hover:scale-105 transition-all  text-teal-950'>Add</li>
-        <li className='hover:scale-105 transition-all  text-teal-950'>Account</li>
+        <li className='hover:scale-105 transition-all text-slate-950 hover:text-teal-400'>Laundry</li>
+        <li className='hover:scale-105 transition-all  text-slate-950 hover:text-teal-400'>Collections</li>
+        <li className='hover:scale-105 transition-all  text-slate-950 hover:text-teal-400'>Add</li>
+        <li className='hover:scale-105 transition-all  text-slate-950 hover:text-teal-400'>Account</li>
       </ul>
       </header>
     </div>
